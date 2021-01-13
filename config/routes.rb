4401956f20eb64c22486917cd2807acae06fb0db
talryan <nataliamryan@gmail.com>
2021-01-13 14:01:41 -0800

    commit tree 2956fde2ae43c72d93fc0fd9e7405748d11b2d51

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'secrets/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+ 
+
 end
